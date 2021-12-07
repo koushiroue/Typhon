22894b52f41f111e9f98da34220f90dbce2c59f7
koushiroue <koushiroue@gmail.com>
2021-12-07 23:21:59 +0800

    bruh

--- pygame/menu.py
+++
@@ -1,8 +0,0 @@
-import pygame, os
-from button import Button
-from config import *
-
-class Menu:
-    def __init__(self):
-        self.startButton = Button("Start", 500, 100, (WIDTH // 2-240, HEIGHT // 2-200), 2)
-        self.exitButton = Button("Exit", 500, 100, (WIDTH // 2-240, HEIGHT // 2 + 100), 2)
