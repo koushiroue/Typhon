4cf45579ced39eb50751efd115abebe7c7290314
koushiroue <koushiroue@gmail.com>
2021-12-11 15:52:57 +0800

    idk

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame
-import thorpy
 
 from game import *
 
 g = Game()
+g.intro()
 
 while g.running:
-    g.intro()
     # display menu
     g.curr_menu.display_menu()
     # loop game(???)
